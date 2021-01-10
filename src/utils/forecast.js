3030e5a6fbaca89eead83e542cb3b10e24fd4716
satyamj933 <37378062+satyamj933@users.noreply.github.com>
2021-01-10 16:24:24 +0530

    add extra feature in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ request({url,json : true},(error,{body}) => {
      }else if (body.error) {
          console.log('Unable to find location',undefined)
      }else{
-     callback(undefined,body.current.weather_descriptions[0] +'. It is currently '+ body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+     callback(undefined,body.current.weather_descriptions[0] +'. It is currently '+ body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '.')
      }
  })
 
